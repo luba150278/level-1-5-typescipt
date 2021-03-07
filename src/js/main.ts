da19cd245b9326625a1c7f98c8eaf60f2c3b21fd
Lyubov Myetolkina <luba150278@gmail.com>
2021-03-07 15:53:42 +0200

    task was finished

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,40 +1,40 @@
-import * as moment from "moment";
-//import * as moment-timer from "moment-timer/lib/";
+import * as moment from 'moment';
+// import * as moment-timer from "moment-timer/lib/";
 
 const buttDown = document.getElementById('timer-manager-down');
 const buttUp = document.getElementById('timer-manager-up');
 const buttStart = document.getElementById('timer-start');
 const timerInstr = document.getElementById('timer-instruction');
-let timer = document.getElementById('time-current-val');
-let timerVal : number = Number(timer.innerHTML);
+const timer = document.getElementById('time-current-val');
+let timerVal = Number(timer.innerHTML);
 
 buttDown.addEventListener('click', () => {
-  if (timerVal != 0) {
-    timerVal--;
+  if (timerVal !== 0) {
+    timerVal -= 1;
     timer.innerHTML = String(timerVal);
   }
-})
+});
 
 buttUp.addEventListener('click', () => {
-  timerVal++;
+  timerVal += 1;
   timer.innerHTML = String(timerVal);
-})
-
-buttStart.addEventListener('click', () => {
-  if (timerVal != 0) {    
-    buttStart.classList.add('hidden');
-    timerInstr.innerHTML = 'Осталось';
-    startTimer(timerVal*60);
-  }
-})
+});
 
 function startTimer(time: number) {
   timer.innerHTML = moment.unix(time).format('mm:ss');
   if (time === 0) {
     buttStart.classList.remove('hidden');
     timerInstr.innerHTML = 'Укажите время в минутах';
-    timer.innerHTML ='0';
+    timer.innerHTML = '0';
     return;
   }
   setTimeout(() => startTimer(time - 1), 1000);
 }
+
+buttStart.addEventListener('click', () => {
+  if (timerVal !== 0) {
+    buttStart.classList.add('hidden');
+    timerInstr.innerHTML = 'Осталось';
+    startTimer(timerVal * 60);
+  }
+});
