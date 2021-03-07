7bdf0117801504a26212b3066a0fc0d222b2d821
Lyubov Myetolkina <luba150278@gmail.com>
2021-03-07 14:50:23 +0200

    timer project was finished

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,8 +1,13 @@
+import * as moment from "moment";
+//import * as moment-timer from "moment-timer/lib/";
+
 const buttDown = document.getElementById('timer-manager-down');
 const buttUp = document.getElementById('timer-manager-up');
 const buttStart = document.getElementById('timer-start');
+const timerInstr = document.getElementById('timer-instruction');
 let timer = document.getElementById('time-current-val');
-let timerVal = Number(timer.innerHTML);
+let timerVal : number = Number(timer.innerHTML);
+
 buttDown.addEventListener('click', () => {
   if (timerVal != 0) {
     timerVal--;
@@ -16,6 +21,20 @@ buttUp.addEventListener('click', () => {
 })
 
 buttStart.addEventListener('click', () => {
-  console.log('start');
+  if (timerVal != 0) {    
+    buttStart.classList.add('hidden');
+    timerInstr.innerHTML = 'Осталось';
+    startTimer(timerVal*60);
+  }
 })
 
+function startTimer(time: number) {
+  timer.innerHTML = moment.unix(time).format('mm:ss');
+  if (time === 0) {
+    buttStart.classList.remove('hidden');
+    timerInstr.innerHTML = 'Укажите время в минутах';
+    timer.innerHTML ='0';
+    return;
+  }
+  setTimeout(() => startTimer(time - 1), 1000);
+}
